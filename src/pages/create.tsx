3ebd4521ade1a497c0868021a634b57b37759206
Ahmet İzgi <dev.ahmetizgi@gmail.com>
2025-10-20 15:00:12 +0300

    Change button text from 'Send send' to 'Send send Zamanla'

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -55,7 +55,7 @@ const Create = () => {
               </Stack>
               <Flex alignItems="flex-end">
                 <Button isLoading={loading} disabled={loading} onClick={getDataFromSocket}>
-                  Send send
+                  Send send Zamanla
                 </Button>
               </Flex>
             </Flex>
